94fb2d13c7db727a0a349ee3de3edae87f15bb6b
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-03-10 20:22:53 -0400

    accounts changes

--- p2/accounts/views/register_view.py
+++ p2/accounts/views/register_view.py
@@ -1,16 +1,20 @@
-from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import User
 
 class RegisterView(APIView):
     def post(self, request):
         username = request.data.get('username')
-        password = request.data.get('password')
+        password1 = request.data.get('password1')
+        password2 = request.data.get('password2')
         email = request.data.get('email')
         first_name = request.data.get('first_name')
         last_name = request.data.get('last_name')
-        if not username or not password or not email:
+        if not username or not password1 or not password2 or not email:
             return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
-        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        if password1 != password2:
+            return Response({'error': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
         return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
